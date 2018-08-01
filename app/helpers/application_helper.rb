1bf60c40d33adc8dd9a192f2559e4cf8bd799486
Mehmet Yılmaz <mehmet3d@hotmail.com>
2018-08-01 22:01:48 +0000

    redcarpet added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
+require 'redcarpet'
+
 module ApplicationHelper
+  class HTMLwithPygments < Redcarpet::Render::HTML
+    def block_code(code, language)
+      Pygments.highlight(code, lexer:language)
+    end
+  end
+
+  def markdown(msg)
+    renderer = HTMLwithPygments.new(hard_wrap: true, filter_html: true)
+    options = {
+      autolink: true,
+      no_intra_emphasis: true,
+      fenced_code_blocks: true,
+      lax_html_blocks: true,
+      strikethrough: true,
+      superscript: true
+    }
+    Redcarpet::Markdown.new(renderer, options).render(msg).html_safe
+  end
 end
